df3692f026c46dfdedb3354f8f6483d71086b629
alvics <alvics71@hotmail.com>
2018-07-23 22:17:54 +1000

    add pages more work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { AboutComponent } from './components/about/about.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { HireComponent } from './components/hire/hire.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
 import { BlogComponent } from './components/blog/blog.component';
-import { WebDesignComponent } from './components/pages/web-design/web-design.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HireComponent } from './components/hire/hire.component';
+import { HomeComponent } from './components/home/home.component';
 import { EcommerceComponent } from './components/pages/ecommerce/ecommerce.component';
+import { SeoComponent } from './components/pages/seo/seo.component';
+import { WebDesignComponent } from './components/pages/web-design/web-design.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,6 +20,7 @@ const routes: Routes = [
   { path: 'hire', component: HireComponent },
   { path: 'webdesign', component: WebDesignComponent },
   { path: 'ecommerce', component: EcommerceComponent },
+  { path: 'seo', component: SeoComponent },
   { path: '**', component: HomeComponent }
 ];
 
--- src/app/components/pages/seo/seo.component.ts
+++ src/app/components/pages/seo/seo.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-seo',
   templateUrl: './seo.component.html',
   styleUrls: ['./seo.component.scss']
 })
-export class SeoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class SeoComponent  {
+title = 'SEO';
+  
 
 }
