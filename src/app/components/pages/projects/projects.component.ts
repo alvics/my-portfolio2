a80254388a7ba2101e865c4301aadf822da3889c
alvics <alvics71@hotmail.com>
2018-08-16 13:41:55 +1000

    add wordpress api layouts in projects

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,3 +1,4 @@
+import { DataService } from './../../../data.service';
 import { HeaderService } from './../../../header.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,18 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   title;
-
+  pages$: Object;
   // Creating a variables as observables to hold the layouts content
   // layouts: Entry<any>[] = [];
   // images: Entry<any>[] = [];
 
   constructor(
-    /*private contenful: ContentfulService*/ private headerService: HeaderService
+    /*private contenful: ContentfulService*/ private headerService: HeaderService,
+    private data: DataService
   ) {}
 
   ngOnInit() {
     // subscribe to the current title observable and set its value equal to the title variable
     this.headerService.currentTitle.subscribe(title => (this.title = title));
+
+    // get layouts from WordPress
+    this.data.getLayouts().subscribe(data => (this.pages$ = data));
+    console.log(this.pages$);
 
     // this.contenful.logContent('3bh985ibn6ee');
     // this.contenful.getContents()
