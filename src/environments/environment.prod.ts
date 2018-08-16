d64a183c3a71a2f429efaa9c1ac67c80f023f8ce
alvics <alvics71@hotmail.com>
2018-08-16 11:55:29 +1000

    ready for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCsNAGVHJHcYrKWs0lKuF4c577BtCmsy7Y',
+    authDomain: 'allen-pavic-wd.firebaseapp.com',
+    databaseURL: 'https://allen-pavic-wd.firebaseio.com',
+    projectId: 'allen-pavic-wd',
+    storageBucket: 'allen-pavic-wd.appspot.com',
+    messagingSenderId: '128420713321'
+  }
 };
