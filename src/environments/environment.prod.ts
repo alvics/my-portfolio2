bbd2b46e33f7bbf8aba34c4d124a651a47e38515
alvics <alvics71@hotmail.com>
2018-08-28 10:17:41 +1000

    api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
- firebase: {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
- }
+  firebase: {
+    apiKey: 'AIzaSyCsNAGVHJHcYrKWs0lKuF4c577BtCmsy7Y',
+    authDomain: 'allen-pavic-wd.firebaseapp.com',
+    databaseURL: 'https://allen-pavic-wd.firebaseio.com',
+    projectId: 'allen-pavic-wd',
+    storageBucket: 'allen-pavic-wd.appspot.com',
+    messagingSenderId: '128420713321'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
- firebase: {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
- }
+  firebase: {
+    apiKey: 'AIzaSyCsNAGVHJHcYrKWs0lKuF4c577BtCmsy7Y',
+    authDomain: 'allen-pavic-wd.firebaseapp.com',
+    databaseURL: 'https://allen-pavic-wd.firebaseio.com',
+    projectId: 'allen-pavic-wd',
+    storageBucket: 'allen-pavic-wd.appspot.com',
+    messagingSenderId: '128420713321'
+  }
 };
 /*
  * In development mode, to ignore zone related error stack frames such as
