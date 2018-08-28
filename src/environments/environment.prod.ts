50d2500ae1fc4bb8b515e09bb0e40c5f834817bb
Allen Pavic <alvics71@hotmail.com>
2018-08-28 10:04:31 +1000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyCsNAGVHJHcYrKWs0lKuF4c577BtCmsy7Y',
-    authDomain: 'allen-pavic-wd.firebaseapp.com',
-    databaseURL: 'https://allen-pavic-wd.firebaseio.com',
-    projectId: 'allen-pavic-wd',
-    storageBucket: 'allen-pavic-wd.appspot.com',
-    messagingSenderId: '128420713321'
-  }
+ firebase: {
+    apiKey: "YOUR_CREDENTIALS_HERE",
+    authDomain: "YOUR_CREDENTIALS_HERE",
+    databaseURL: "YOUR_CREDENTIALS_HERE",
+    projectId: "YOUR_CREDENTIALS_HERE",
+    storageBucket: "YOUR_CREDENTIALS_HERE",
+    messagingSenderId: "YOUR_CREDENTIALS_HERE"
+ }
 };
